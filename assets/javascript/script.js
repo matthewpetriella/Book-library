88dfc37449c7d0ee882af4480b67d8b3b005403f
Chris <chrisbracewell35@gmail.com>
2022-01-17 23:37:13 -0500

    adding youtube api and modal functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,10 @@
 var modal = document.querySelector(".modal");
 
  //Get the button that opens the modal
-var openBtn = document.querySelector(".nysearch");
+var openBtn = document.querySelector("#nysearch");
+
+//Get the button that opens the modal
+var nonFictinoalBtn = document.querySelector("#nySearchNfictional");
 
 // Get the  element that closes the modal
 var bookBtn = document.querySelector(".book-search")
@@ -10,28 +13,27 @@ var bookBtn = document.querySelector(".book-search")
 // get modal cancel button
 var cancelBtn = document.querySelector(".cancel-search");
 
-// defining variables
-var bookList = document.querySelector('ul');
-var fetchButton = document.getElementById('bestsellerbtn');
+// defining  variables
+var fictionalList = document.querySelector("#fiction-list");
+
+var nFictionalList = document.querySelector("#nonfictional-list");
+
+
 
 // API Key
-var bookAPI= "D9OwWTZWlrbbFIzrqzKyzY9zxhC4MVua"
-var youtubeAPI = "AIzaSyDLwFd4zYweemWMyBrFsyroycgBQVZWCmw"
+var bookAPI= "D9OwWTZWlrbbFIzrqzKyzY9zxhC4MVua";
+var youtubeAPIKey = "AIzaSyCnm1Vk0t1Po9Fanm2-OIFvOP4HzN4SUCM";
 
 
 
 
-// modal.addEventListener('load', function(){
-//    modal.style.display = "block"
-// });
 
 //When the user clicks the cancel button, close the modal
-cancelBtn.addEventListener('click', function (){
-modal.style.display = "none"
+// cancelBtn.addEventListener('click', function (){
+// modal.style.display = "none"
 
-});
+// });
 
-// When the modal search button is clicked run Save search function
 
 // bookBtn.addEventListener('click', function(event,userSearch){
 //     event.preventDefault();
@@ -44,6 +46,7 @@ modal.style.display = "none"
 
 
 function getApi() {
+  modal.style.display = "none"
   var requestUrl = "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=" + bookAPI;
   fetch(requestUrl)
   .then(function(response) {
@@ -53,19 +56,71 @@ function getApi() {
   .then(function(data) {
     console.log (data)
     for (var i = 0; i < 5; i++) {
-     var listItem = document.createElement('li');
+     var author = document.createElement('h2');
+      author.textContent = data.results.lists[0].books[i].author + ":  ";
+      fictionalList.appendChild(author); 
+        
+      var listItem = document.createElement('a');
       listItem.textContent = data.results.lists[0].books[i].title;
-      bookList.appendChild(listItem);
-
-      var author = document.createElement('p');
-      author.textContent = data.results.lists[0].books[i].author;
-      listItem.appendChild(author);
+      author.appendChild(listItem);
+      listItem.classList.add("button-"+ [i]);
     }
+    
   });
 };
+
+
+function getApi2() {
+  modal.style.display = "none"
+  var requestUrl = "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=" + bookAPI;
+  fetch(requestUrl)
+  .then(function(response) {
+    console.log (response)
+    return response.json();
+  })
+  .then(function(data) {
+    console.log (data)
+    for (var i = 0; i < 5; i++) {
+     var nauthor = document.createElement('h2');
+      nauthor.textContent = data.results.lists[1].books[i].author + ":  ";
+      nFictionalList.appendChild(nauthor); 
+        
+      var nlistItem = document.createElement('a');
+      nlistItem.textContent = data.results.lists[1].books[i].title;
+      nauthor.appendChild(nlistItem);
+      nlistItem.classList.add("button-"+ [i]);
+
+    }
+ 
+  });
+  
+};
+
+
+
+
+// function youtubeAPI() {
+//   modal.style.display = "none"
+//   var requestUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&topicId=IT%20ENDS%20WITH%20US&type=video&maxResults=5&key=" + youtubeAPIKey;
+//   fetch(requestUrl)
+//   .then(function(response) {
+//     console.log (response)
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log (data)
+//     for (var i = 0; i < 5; i++) {
+     
+//     }
+//   });
+// };
+
 
 
 var search = {};
 
 // // When the user clicks the button, open the modal
 openBtn.addEventListener('click', getApi);
+
+// 
+nonFictinoalBtn.addEventListener('click', getApi2);
